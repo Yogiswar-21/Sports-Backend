114890d16a5a79174ab6ae0ce904cf63ff7d56d4
Yogiswar-21 <kotayogi2004@gmail.com>
2025-09-13 12:00:13 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://sports-backend-wxdt.onrender.com/auth/google/callback`, // Use env variable for deployment
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`, // Use env variable for deployment
     },
     async (accessToken, refreshToken,profile, done) => {
       try {
